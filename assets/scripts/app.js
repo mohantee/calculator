c8706ef5f4361502579d255cc16945373497a390
Saurav <tekkzera@gmail.com>
2021-08-29 11:33:19 +0530

    make the site mobile responsive

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,4 @@
+//create DOM bindings
 const btnNumbers = Array.from(document.querySelectorAll('.number'));
 const btnOperators = Array.from(document.querySelectorAll('.operator'));
 const btnAllClear = document.querySelector('.all-clear');
@@ -11,13 +12,16 @@ let operandOne = 0;
 let operandTwo = 0;
 let shouldCalculate = true;
 let shouldResetDisplay = false;
+let shouldResetLog = false;
 
+//functions to perform basic arithematics
 const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
 const modulo = (num1, num2) => num1 % num2;
 
+//fucntion to perform operation between operands
 const operate = (operator, num1, num2) => {
 	if (operator === '+') {
 		return add(num1, num2);
@@ -32,16 +36,21 @@ const operate = (operator, num1, num2) => {
 	}
 };
 
+//btnAllClearHandler - when the user hits AC button, clear the text content
+//and operand log array.
 const setDisplayToZero = () => {
 	display.textContent = '0';
-	operandOne = operandTwo = 0;
 	operandLog = [];
 };
 
+//clear display content
 const resetDisplay = () => {
 	display.textContent = '';
 };
 
+//backspace functionality - when user clicks 'C' button, delete the last
+//character from the display.textContent string if it's length is more
+//than one, otherwise set it to zero
 const btnClearHandler = () => {
 	if (shouldCalculate && display.textContent.length > 1)
 		display.textContent = display.textContent.slice(0, -1);
@@ -49,18 +58,28 @@ const btnClearHandler = () => {
 };
 
 const btnEqualsHandler = () => {
-	// prevent the user from pressing equals repeatedly
+	// if shouldcalculate is true, evaluate the if block. This prevents the
+	//user from evaluating the operandLog repeatedly by clicking on equals.
+	//Set it to false before function return. Set it to true after a number
+	//has been appended to display area.
 	if (shouldCalculate) {
 		operandOne = operandLog[0];
 		operandTwo = Number.parseFloat(display.textContent);
 		display.textContent = operate(operatorLog[0], operandOne, operandTwo);
 		operandLog.unshift(Number.parseFloat(display.textContent));
+
+		operandOne = operandTwo = 0;
 	}
 	shouldCalculate = false;
 	shouldResetDisplay = true;
+	shouldResetLog = true;
 };
 
+//function to populate the display area
 const appendNumber = value => {
+	if (shouldResetLog) {
+		operandLog = [];
+	}
 	if (display.textContent === '0' || shouldResetDisplay) {
 		resetDisplay();
 		shouldResetDisplay = false;
@@ -69,8 +88,8 @@ const appendNumber = value => {
 	shouldCalculate = true;
 };
 
+//calculator button click logic
 const calculatorLogic = operator => {
-	let displayValue = Number.parseFloat(display.textContent);
 	operatorLog.unshift(operator);
 	if (operandLog.length === 0 && shouldCalculate) {
 		operandLog.unshift(Number.parseFloat(display.textContent));
@@ -90,8 +109,10 @@ const calculatorLogic = operator => {
 	}
 	shouldResetDisplay = true;
 	shouldCalculate = false;
+	shouldResetLog = false;
 };
 
+//bind event listeners to all the buttons
 btnNumbers.forEach(btn => {
 	btn.addEventListener('click', () => {
 		appendNumber(btn.textContent);
