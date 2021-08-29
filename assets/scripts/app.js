c0ca2d6e7ec492383acc48e481bbbdba771790e1
Saurav <tekkzera@gmail.com>
2021-08-29 14:08:25 +0530

    fix display overflow bug

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -18,7 +18,7 @@ let shouldResetLog = false;
 const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
-const divide = (num1, num2) => num1 / num2;
+const divide = (num1, num2) => (num1 / num2).toFixed(5);
 const modulo = (num1, num2) => num1 % num2;
 
 //fucntion to perform operation between operands
